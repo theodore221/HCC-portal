083b372f1196ba0f8336cb949f4805ed12a46d2d
Theodore Xavier <theodore.xavier221@gmail.com>
2025-10-21 16:31:44 +1100

    first commit

---
+++ lib/supabase.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const sbBrowser = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+export const sbServer = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: { persistSession: false },
+    }
+  );
