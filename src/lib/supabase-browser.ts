546d1a7411a737b18ec851b77fddf7a4d33497f6
Theodore Xavier <32845110+theodore221@users.noreply.github.com>
2025-10-31 01:21:21 +1100

    Fix Supabase browser client import

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -1,11 +1,8 @@
-import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import type { Database } from "@/lib/database.types";
 
-const browserClient = createBrowserClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const browserClient = createClientComponentClient<Database>();
 
 export function sbBrowser() {
   return browserClient;
